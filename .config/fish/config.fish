67b985198cd50cd6c2e20ea6cf381346806a00e9
DawidekSzopen <62569747+dawidekszopen@users.noreply.github.com>
2024-09-21 23:13:28 +0200

    add fish +

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+set -g theme_display_vi no
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
+set -g theme_title_display_process no
+set -g theme_title_display_path no
+set -g theme_title_display_user no
+set -g theme_title_use_abbreviated_path no
+set -g theme_avoid_ambiguous_glyphs no
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+set -g theme_display_jobs_verbose no
+set -g default_user your_normal_user
+set -g theme_color_scheme grovbox
+
+function fish_greeting
+
+end
